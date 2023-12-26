1ba9f8aa564f8382d87f2ac98040656b4a3935e8
Ezugo23 <134660097+Ezugo23@users.noreply.github.com>
2023-12-26 15:04:33 +0100

    clear

--- src/component/Password.js
+++ src/component/Password.js
@@ -8,53 +8,6 @@ export default function Password() {
         <h3 className='font-bold mt-3 text-left mx-3'>Change Password</h3>
       
      <div className='d-lg-flex mx-10'>
-     {/*<Form className='mr-20 mt-10'>
-      <div className='row align-items-center'>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Old Password</Form.Label>
-        <input
-                  type="password"
-                  name="old-password"
-                  id="old-password"
-                  placeholder="Name"
-                  autoComplete="family-name"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" style={{ paddingLeft: '10px' }}
-                />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>New Password</Form.Label>
-        <input
-                  type="password"
-                  name="new-password"
-                  id="new-password"
-                  placeholder="Phone Number"
-                  autoComplete="family-name"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" style={{ paddingLeft: '10px' }}
-                />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Confirm Password</Form.Label>
-        <input
-                  type="password"
-                  name="confirm-password"
-                  id="confirm-password"
-                  placeholder="Email Address"
-                  autoComplete="family-name"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" style={{ paddingLeft: '10px' }}
-                />
-      </Form.Group>
-      <div className='mx-30 col-lg-2'>
-      <button
-          type="submit"
-          className="block mx-auto m-2 mt-6 bg-cyan-600 hover:bg-cyan-700 text-white font-bold py-2 px-20 rounded"
-        >
-           Change
-        </button>
-        </div>
-
-   </div>
-      
-  </Form>*/}
 
 <Form class='custom-form mr-20 mt-10'>
     <div class='custom-row align-items-center'>
